9ff0b930d07284bf32170a5e0d84f37f906b16e4
Troy <130083210+psycho-daisies@users.noreply.github.com>
2024-01-27 13:39:34 -0800

    Update hw3_stack_linkedlist.py
    Updated / Organized some stuff
    - added comments
    - print statements for exceptions

--- Homework/Week3/hw3_stack_linkedlist.py
+++ Homework/Week3/hw3_stack_linkedlist.py
@@ -12,29 +12,24 @@ Implement a stack for both a Linked List & Array. Use your HW 1 implementation o
     Peek
 """
 
-"""
-Stack / Linked List
-Troy Brunette
-CS240
 
-Implement a stack for both a Linked List & Array. Use your HW 1 implementation of a Linked List.
-
-    Push
-    Pop
-    IsEmpty
-    IsFull
-    Peek
-"""
-from node import Node
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 
 class Stack:
-    def __init__(self):
+    def __init__(self, max_size=None):
         self.head = None
         self.size = 0
+        self.max_size = max_size
 
-    # Add a new node to the front of list
+    # Push adds an item to the top of the Stack; except if the Stack is full
     def Push(self, data):
+        if self.size == self.max_size:
+            return print(f"Can't add {data} to a full stack! ")
+
         node = Node(data=data)
         self.size += 1
         if self.head is None:
@@ -44,28 +39,66 @@ class Stack:
             self.head = node
             self.head.next = node.next
 
-    # Removes the first node
+    # Pop removes from the top of the stack; the first node of the list
     def Pop(self):
         if self.head is None:
-            return
+            return print("Can't Pop! ~ Stack is Empty!")
         self.size = self.size - 1
         popped = self.head
         self.head = self.head.next
         return popped.data
 
+    # Returns True if the Stack is empty
     def IsEmpty(self):
-        pass
+        return self.head is None
 
+    # Returns True if the Stack is full by checking size of Linked List to max Stack size
     def IsFull(self):
-        pass
+        return self.max_size == self.size
 
+    # Peek looks at the top of Stack but does not Pop
     def Peek(self):
-        pass
+        if self.head is None:
+            return print("Can't Peek! ~ Stack is Empty")
+        return self.head.data
+
+    # Returns the size of the Stack
+    def get_size(self):
+        return self.size
+
+    # Counts each node in the Stack and returns the size
+    def count_nodes(self):
+        current = self.head
+        counter = 0
+        if current is None:
+            return 0
+        else:
+            while current:
+                counter += 1
+                current = current.next
+        self.size = counter  # update size
+        return int(counter)
+
 
 # Testing the Stack functions
-test_stack = Stack()
+test_stack = Stack(5)
+print(test_stack.IsEmpty())
+print(f"Current size of Stack: ", test_stack.get_size())
 test_stack.Push(1)
 test_stack.Push(2)
 test_stack.Push(3)
-
-print(test_stack.Pop())
+test_stack.Push(4)
+print(f"Is the Stack full? ", test_stack.IsFull())
+test_stack.Push(5)
+test_stack.Push(6)
+print(f"Current size of Stack: ", test_stack.get_size())
+print(f"Is the Stack Empty? ", test_stack.IsEmpty())
+print(f"Is the Stack full? ", test_stack.IsFull())
+print("Take a PEEK at the top of the stack:", test_stack.Peek())
+print("Popped item: ", test_stack.Pop())
+print("Take a PEEK at the top of the stack:", test_stack.Peek())
+print("Popped item: ", test_stack.Pop())
+print("Take a PEEK at the top of the stack:", test_stack.Peek())
+print("Popped item: ", test_stack.Pop())
+print(f"Is the Stack Empty? ", test_stack.IsEmpty())
+print(f"Current size of Stack: ", test_stack.get_size())
