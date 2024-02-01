b7de5756c5d7ba75bcaa9b77e40335e53a5be59f
Troy <130083210+psycho-daisies@users.noreply.github.com>
2024-02-01 12:07:58 -0800

    Create mergesort.py

---
+++ Homework/Week4/mergesort.py
@@ -0,0 +1,63 @@
+# Merge Sort Assignment
+# By: Troy Brunette
+
+from res.binarysearch import binary_search
+from res.importfile import read_file, print_sorted
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    return merge(arr, left, right)
+
+
+def merge(arr, left, right):
+    i = j = k = 0
+
+    # Compares the sub-arrays and puts them in sorted order
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    # Merges the sub-arrays
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    # Copy the remaining elements
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+    return arr
+
+
+# Testing Merge Sort on a small array
+numbers = [2, 9, 6, 3, 7, 5, 8, 4, 1]
+print(merge_sort(numbers))
+
+# Testing Merge Sort on provided numbers text file
+# Searching for specified numbers using Binary Search
+unsorted = read_file("res/numbers-4.txt")
+merge_sort(unsorted)
+print_sorted(unsorted)
+target = 11559
+target2 = 90262
+index, count = binary_search(unsorted, target)
+index2, count2 = binary_search(unsorted, target2)
+print(f"Target: {target} at Index: {index}")
+print(f"Target: {target2} at Index: {index2}")
