38b2111a2375ad8555a56bf38f6b255ee7cc260b
Troy <130083210+psycho-daisies@users.noreply.github.com>
2024-03-13 14:36:19 -0700

    Update bfs_dfs.py

--- Homework/HW_BFS_DFS/bfs_dfs.py
+++ Homework/HW_BFS_DFS/bfs_dfs.py
@@ -1,5 +1,5 @@
-from homework.queue import Queue
-from homework.read_from_file import get_graph, get_neighbors
+from Homework.queue import Queue
+from Homework.read_from_file import get_graph, get_neighbors
 
 
 def breadth_fist_search(graph, start):
